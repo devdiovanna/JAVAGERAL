07a83d417a2b6e4fa2074d238e520acedb8ab9ac
Diovana Ramos <devdiovanna@gmail.com>
2023-10-07 17:40:24 -0300

    Adicionado validação de alunos aprovados!

---
+++ Aprendendojava/src/AlunosReprovados.java
@@ -0,0 +1,18 @@
+public class AlunosReprovados {
+    public static void main(String[] args) {
+        // write your code here
+
+        int nota = 50;
+
+// nota maior ou igual 70, então aprovado
+        // if-else
+        if (nota >= 70) {
+            System.out.println("Aluno aprovado!");
+        } else {
+            System.out.println("Não aprovado");
+
+        }
+
+    }
+}
+
