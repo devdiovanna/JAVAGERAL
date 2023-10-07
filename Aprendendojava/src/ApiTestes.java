473b4cdbe65004b3e2f587a378fdfb8c9579c0af
Diovana Ramos <devdiovanna@gmail.com>
2023-10-07 20:53:31 -0300

    nota de alunos não aprovados

---
+++ Aprendendojava/src/ApiTestes.java
@@ -0,0 +1,20 @@
+public class ApiTestes {
+
+    // API para gerar números aleatórios
+
+    // Classe para gerar números aleatórios
+    public class RandomNumberGenerator {
+
+        // Função para gerar um número aleatório entre 0 e 100
+        public int generateRandomNumber() {
+            return (int) Math.floor(Math.random() * 100);
+        }
+
+        // Método para retornar os dados da API
+        public String getRandomNumber() {
+            return "{" +
+                    "\"number\": " + generateRandomNumber() +
+                    "}";
+        }
+    }
+}
