b65940e4eb41c6fcb210d2df4d5da4cd8b6dfbf7
Diovana Ramos <devdiovanna@gmail.com>
2023-10-07 17:47:40 -0300

    Funções

---
+++ Aprendendojava/src/Funcoes.java
@@ -0,0 +1,12 @@
+public class Funcoes {
+    public static void main(String[] args) {
+        int result = soma(2, 3);
+        System.out.println(result);
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
+
+    }
+}
+
