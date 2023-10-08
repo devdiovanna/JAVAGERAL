5a07e5cbe123eca213d72eda9d32e999199bd7ef
Diovana Ramos <devdiovanna@gmail.com>
2023-10-08 10:00:08 -0300

    nota de alunos não aprovados

---
+++ Aprendendojava/src/SwingeJavafx.java
@@ -0,0 +1,7 @@
+package SwingeJavafx;
+    public  class SwingeJavafx {
+    public static void main(String[] args) {
+    float nota = 8.5f;
+        System.out.printf("A nota é " + nota);
+        }
+}
