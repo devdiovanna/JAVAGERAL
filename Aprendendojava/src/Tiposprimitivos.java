d3097891bdab11845409fb615392f34d5a84f78a
Diovana Ramos <devdiovanna@gmail.com>
2023-10-08 10:18:54 -0300

    nota de alunos não aprovados

---
+++ Aprendendojava/src/Tiposprimitivos.java
@@ -0,0 +1,27 @@
+/*
+teste
+fgfhg
+hghghg
+ */
+package Tiposprimitivos;
+
+
+
+
+
+public class Tiposprimitivos {
+
+
+
+
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float m = n1 + n2/2;
+        System.out.printf("A média é igual a " + m);
+
+}
+
+
+
+}
