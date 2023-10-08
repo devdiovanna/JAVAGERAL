ce0cc96d944ea7a947ecf11ae51562bd141f26c4
Diovana Ramos <devdiovanna@gmail.com>
2023-10-08 11:16:13 -0300

    nota de alunos não aprovados

---
+++ Aprendendojava/src/Aritimeticas.java
@@ -0,0 +1,9 @@
+public class Aritimeticas {
+
+    public static void main(String[] args) {
+        double ale = Math.random();
+        int n = (int) (5 + ale * (50 - 15));
+        System.out.println(n);
+
+    }
+}
