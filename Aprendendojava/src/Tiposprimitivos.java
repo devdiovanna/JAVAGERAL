e9fb46bc451cbc9f11a37ffafa4108b527fb7efd
Diovana Ramos <devdiovanna@gmail.com>
2023-10-08 10:53:50 -0300

    nota de alunos não aprovados

--- Aprendendojava/src/Tiposprimitivos.java
+++ Aprendendojava/src/Tiposprimitivos.java
@@ -15,13 +15,16 @@ public class Tiposprimitivos {
 
 
     public static void main(String[] args) {
-        int n1 = 3;
+        /*int n1 = 3;
         int n2 = 5;
         float m = n1 + n2/2;
-        System.out.printf("A média é igual a " + m);
-
-}
+        System.out.printf("A média é igual a " + m);*/
 
 
+        int numero = 5;
+        int valor = 5 + numero++;
+        System.out.printf("valor");
+
+    }
 
 }
